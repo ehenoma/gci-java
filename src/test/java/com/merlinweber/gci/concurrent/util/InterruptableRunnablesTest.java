a0189bf2a42554678e2675084f9127b7ef6dad78
Merlin O <merlinosayimwen@gmail.com>
2018-10-25 11:53:41 +0200

    Add test coverage for the InterruptableRunnables util.

---
+++ src/test/java/com/merlinweber/gci/concurrent/util/InterruptableRunnablesTest.java
@@ -0,0 +1,30 @@
+package com.merlinweber.gci.concurrent.util;
+
+import static com.google.common.util.concurrent.Runnables.doNothing;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class InterruptableRunnablesTest {
+
+  @Test
+  void checkNoThrow_nothrow() {
+    InterruptableRunnable action = InterruptableRunnables.nothrow(doNothing());
+
+    try {
+      action.run();
+    } catch (InterruptedException interruption) {
+      fail("InterruptableRunnable of type nothrow throws an InterruptedException.");
+    }
+  }
+
+  @Test
+  void checkThrow_immediate() {
+    InterruptableRunnable action = InterruptableRunnables.immediate();
+
+    assertNotNull(action);
+    assertThrows(InterruptedException.class, action::run);
+  }
+}
