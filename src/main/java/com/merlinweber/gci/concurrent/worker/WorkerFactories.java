4c278abe7426a9bb3451031bb1bf7a165a3a8f6c
Merlin O <merlinosayimwen@gmail.com>
2018-10-25 17:50:03 +0200

    Add util class that provides factory methods to create WorkerFactories.

---
+++ src/main/java/com/merlinweber/gci/concurrent/worker/WorkerFactories.java
@@ -0,0 +1,64 @@
+package com.merlinweber.gci.concurrent.worker;
+
+import com.google.common.util.concurrent.MoreExecutors;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Util class that provides some factory methods for creating WorkerFactories.
+ *
+ * @see WorkerFactory
+ * @see ConfigurableWorkerFactory
+ */
+public final class WorkerFactories {
+
+  // Prevent manual creation.
+  private WorkerFactories() {}
+
+  /**
+   * Returns a factory that creates workers which all
+   * run in one explicitly created thread.
+   */
+  public static ConfigurableWorkerFactory singleThreadExecuted() {
+    return explicitlyExecuted(Executors.newSingleThreadExecutor());
+  }
+
+  /**
+   * Returns a factory that creates workers which all
+   * work in their own explicitly created thread.
+   */
+  public static ConfigurableWorkerFactory exclusiveThreadPerWorker() {
+    return explicitlyExecuted(task -> {
+      Thread delegate = new Thread(task);
+      delegate.start();
+    });
+  }
+
+  /** Returns an factory that creates workers which work in the current thread. */
+  public static ConfigurableWorkerFactory synchronous() {
+    return explicitlyExecuted(MoreExecutors.directExecutor());
+  }
+
+  /**
+   * Returns a factory that uses the given executor to start its workers.
+   * @param executor Explicit executor that starts workers.
+   */
+  public static ConfigurableWorkerFactory explicitlyExecuted(Executor executor) {
+    return new ExplicitlyExecutedFactory(executor);
+  }
+
+  /* Delegates an executor to create a factory. */
+  private static class ExplicitlyExecutedFactory extends AbstractWorkerFactory {
+    private Executor executor;
+
+    private ExplicitlyExecutedFactory(Executor executor) {
+      super();
+      this.executor = executor;
+    }
+
+    @Override
+    public Worker getInstance(String name) {
+      return getInstance(name, executor);
+    }
+  }
+}
