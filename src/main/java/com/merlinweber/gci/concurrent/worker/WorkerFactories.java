7609abca43fb69ec2226d027eb7e06d163f9c7f5
Merlin O <merlinosayimwen@gmail.com>
2018-10-25 21:33:16 +0200

    Override some object methods to make factory implementation unique.

--- src/main/java/com/merlinweber/gci/concurrent/worker/WorkerFactories.java
+++ src/main/java/com/merlinweber/gci/concurrent/worker/WorkerFactories.java
@@ -1,6 +1,5 @@
 package com.merlinweber.gci.concurrent.worker;
 
-import com.google.common.util.concurrent.MoreExecutors;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -15,27 +14,25 @@ public final class WorkerFactories {
   // Prevent manual creation.
   private WorkerFactories() {}
 
-  /**
-   * Returns a factory that creates workers which all
-   * run in one explicitly created thread.
-   */
+  /** Returns a factory that creates workers which all run in one explicitly created thread. */
   public static ConfigurableWorkerFactory singleThreadExecuted() {
     return explicitlyExecuted(Executors.newSingleThreadExecutor());
   }
 
   /**
-   * Returns a factory that creates workers which all
-   * work in their own explicitly created thread.
+   * Returns a factory that creates workers which all work in their own explicitly created thread.
    */
   public static ConfigurableWorkerFactory exclusiveThreadPerWorker() {
-    return explicitlyExecuted(task -> {
-      Thread delegate = new Thread(task);
-      delegate.start();
-    });
+    return explicitlyExecuted(
+        task -> {
+          Thread delegate = new Thread(task);
+          delegate.start();
+        });
   }
 
   /**
    * Returns a factory that uses the given executor to start its workers.
+   *
    * @param executor Explicit executor that starts workers.
    */
   public static ConfigurableWorkerFactory explicitlyExecuted(Executor executor) {
@@ -54,6 +51,16 @@ public final class WorkerFactories {
     @Override
     public Worker getInstance(String name) {
       return getInstance(name, executor);
+    }
+
+    @Override
+    public boolean equals(Object value) {
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      return 0;
     }
   }
 }
